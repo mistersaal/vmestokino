03b1af531395e59aea5f7af5ae76de9b1b71825d
Сааль Степан <mister.saal@yandex.ru>
2020-05-07 02:02:50 +0300

    Set up frontend dependencies.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,41 +1,36 @@
-window._ = require('lodash');
+import _ from 'lodash';
+window._ = _;
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
-} catch (e) {}
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
+import axios from 'axios';
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-Vk-Auth-Url'] = window.location.href;
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+import Vue from 'vue';
+window.Vue = Vue;
+import VueRouter from 'vue-router'
+window.VueRouter = VueRouter;
+Vue.use(VueRouter)
 
-// import Echo from 'laravel-echo';
+import Echo from "laravel-echo"
 
-// window.Pusher = require('pusher-js');
+import io from 'socket.io-client';
+window.io = io;
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001',
+    auth: {
+        headers: {
+            'X-Vk-Auth-Url': window.location.href
+        }
+    }
+});
+
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+Vue.use(Buefy)
+
+import bridge from '@vkontakte/vk-bridge';
+window.bridge = bridge;
