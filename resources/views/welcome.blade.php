7a906eff0c59d5ccbca6949330a4456e71667b60
Сааль Степан <mister.saal@yandex.ru>
2020-05-28 02:26:27 +0300

    Revert "Fix chat #4 for iphone X."
    This reverts commit 84ce78de

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="ru" style="overflow: hidden;">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=contain">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, viewport-fit=cover">
         <title>Вместо кино</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
