241929dd2c38a7fb6604ef14131e86c9c87edc14
Екатерина Агапова <katena.agapova.01@mail.ru>
2020-06-14 01:33:55 +0300

    Debug

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -25,6 +25,7 @@
             bridge.subscribe(e => {
                 if (e.detail.type === 'VKWebAppUpdateConfig') {
                     const scheme = e.detail.data.scheme ? e.detail.data.scheme : 'client_light';
+                    alert(scheme);
                     if (scheme !== 'client_light') {
                         document.getElementById('dark_scheme').media = "";
                         document.getElementById('light_scheme').media = "none";
